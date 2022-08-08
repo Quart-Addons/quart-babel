3a13f5a7fc92e0fcdaad4a79db129d362eb8d99e
Chris Rood <crood58@gmail.com>
2022-08-08 23:14:10 +0000

    Converted to pytest.

--- tests/test_date_formatting.py
+++ tests/test_date_formatting.py
@@ -1,160 +1,200 @@
 """
-Tests date formatting.
+Tests date and time formatting
+with Quart Babel.
 """
 import asyncio
 from datetime import datetime, timedelta
-import aiounittest
+from time import time
+import pytest
 
 from quart import Quart
-import quart_babel as babel_ext
+from quart_babel import (Babel, format_date, format_datetime, format_timedelta,
+                        refresh, force_locale, get_locale)
 from quart_babel.utils.formats import _get_format
 
-class DateFormattingTestCase(aiounittest.AsyncTestCase):
+@pytest.mark.asyncio
+async def test_basic():
+    """
+    Tests basic functions for
+    date and time functions.
+    """
+    app = Quart(__name__)
+    Babel(app)
 
-    async def test_basics(self):
-        app = Quart(__name__)
-        babel_ext.Babel(app)
-        d = datetime(2010, 4, 12, 13, 46)
-        delta = timedelta(days=6)
+    date = datetime(2022, 8, 8, 17, 46)
+    delta = timedelta(days=6)
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
-            assert babel_ext.format_date(d) == 'Apr 12, 2010'
-            assert babel_ext.format_time(d) == '1:46:00 PM'
-            assert babel_ext.format_timedelta(delta) == '1 week'
-            assert babel_ext.format_timedelta(delta, threshold=1) == '6 days'
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == 'Aug 8, 2022, 5:46:00 PM'
+        assert format_date(date) == 'Aug 8, 2022'
+        assert time(date) == '5:46:00 PM'
+        assert format_timedelta(delta, threshold=1) == '6 days'
 
-        async with app.test_request_context("/"):
-            app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europe/Vienna'
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 3:46:00 PM'
-            assert babel_ext.format_date(d) == 'Apr 12, 2010'
-            assert babel_ext.format_time(d) == '3:46:00 PM'
+    async with app.test_request_context("/"):
+        app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europene/Vienna'
+        assert format_datetime(date) == 'Aug 8, 2022, 11:46:00 PM'
+        assert format_date(date) == 'Aug 8, 2022'
+        assert time(date) == '11:46:00 PM'
 
-        async with app.test_request_context("/"):
-            app.config['BABEL_DEFAULT_LOCALE'] = 'de_DE'
-            assert babel_ext.format_datetime(d, 'long') == \
-                '12. April 2010 um 15:46:00 MESZ'
-    
-    async def test_init_app(self):
-        b = babel_ext.Babel()
-        app = Quart(__name__)
-        b.init_app(app)
-        d = datetime(2010, 4, 12, 13, 46)
+    async with app.test_request_context("/"):
+        app.config['BABEL_DEFAULT_LOCALE'] = 'de_DE'
+        assert format_datetime(date, 'long') == \
+            '8. Aug 8 um 2022, 23:46:00 MESZ'
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
-            assert babel_ext.format_date(d) == 'Apr 12, 2010'
-            assert babel_ext.format_time(d) == '1:46:00 PM'
+@pytest.mark.asyncio
+async def test_init_app():
+    """
+    Tests basic functions for
+    date and time functions using
+    `quart_babel.init_app`.
+    """
+    babel = Babel()
+    app = Quart(__name__)
+    babel.init_app(app)
 
-        async with app.test_request_context("/"):
-            app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europe/Vienna'
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 3:46:00 PM'
-            assert babel_ext.format_date(d) == 'Apr 12, 2010'
-            assert babel_ext.format_time(d) == '3:46:00 PM'
+    date = datetime(2022, 8, 8, 17, 46)
 
-        async with app.test_request_context("/"):
-            app.config['BABEL_DEFAULT_LOCALE'] = 'de_DE'
-            assert babel_ext.format_datetime(d, 'long') == \
-                '12. April 2010 um 15:46:00 MESZ'
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == 'Aug 8, 2022, 5:46:00 PM'
+        assert format_date(date) == 'Aug 8, 2022'
+        assert time(date) == '5:46:00 PM'
 
-    async def test_custom_formats(self):
-        app = Quart(__name__)
-        app.config.update(
-            BABEL_DEFAULT_LOCALE='en_US',
-            BABEL_DEFAULT_TIMEZONE='Pacific/Johnston'
-        )
-        b = babel_ext.Babel(app)
-        b.date_formats['datetime'] = 'long'
-        b.date_formats['datetime.long'] = 'MMMM d, yyyy h:mm:ss a'
+    async with app.test_request_context("/"):
+        app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europene/Vienna'
+        assert format_datetime(date) == 'Aug 8, 2022, 11:46:00 PM'
+        assert format_date(date) == 'Aug 8, 2022'
+        assert time(date) == '11:46:00 PM'
 
-        b.date_formats['date'] = 'long'
-        b.date_formats['date.short'] = 'MM d'
+    async with app.test_request_context("/"):
+        app.config['BABEL_DEFAULT_LOCALE'] = 'de_DE'
+        assert format_datetime(date, 'long') == \
+            '8. Aug 8 um 2022, 23:46:00 MESZ'
 
-        d = datetime(2010, 4, 12, 13, 46)
+@pytest.mark.asyncio
+async def test_custom_formates():
+    """
+    Tests custom date and time formats.
+    """
+    app = Quart(__name__)
+    app.config.update(
+        BABEL_DEFAULT_LOCALE='en_us',
+        BABEL_DEFAULT_TIMEZONE='Pacific/Johnston'
+    )
+    babel = Babel(app)
+    babel.date_formats['datetime'] = 'long'
+    babel.date_formats['datetime.long'] = 'MMMM d, yyyy h:mm:ss a'
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == 'April 12, 2010 3:46:00 AM'
-            assert _get_format('datetime') == 'MMMM d, yyyy h:mm:ss a'
-            # none; returns the format
-            assert _get_format('datetime', 'medium') == 'medium'
-            assert _get_format('date', 'short') == 'MM d'
+    babel.date_formats['date'] = 'long'
+    babel.date_formats['short'] = 'MM d'
 
-    async def test_custom_locale_selector(self):
-        app = Quart(__name__)
-        b = babel_ext.Babel(app)
-        d = datetime(2010, 4, 12, 13, 46)
+    date = datetime(2022, 8, 8, 17, 46)
 
-        the_timezone = 'UTC'
-        the_locale = 'en_US'
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == 'August 8, 2022 5:46:00 PM'
+        assert _get_format('datetime') == 'MMMM d, yyyy h:mm:ss a'
+        # none, returns the format
+        assert _get_format('datetime', 'medium') == 'medium'
+        assert _get_format('date', 'short') == 'MM d'
 
-        @b.localeselector
-        def select_locale():
-            return the_locale
+@pytest.mark.asyncio
+async def test_custom_locale_selector():
+    """
+    Tests a custom locale selector.
+    """
+    app = Quart(__name__)
+    babel = Babel(app)
 
-        @b.timezoneselector
-        def select_timezone():
-            return the_timezone
+    date = datetime(2022, 8, 8, 17, 46)
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
+    the_locale = 'en_US'
+    the_timezone = 'UTC'
 
-        the_locale = 'de_DE'
-        the_timezone = 'Europe/Vienna'
+    @babel.localeselector
+    def select_locale():
+        return the_locale
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == '12.04.2010, 15:46:00'
+    @babel.timezoneselector
+    def select_timezone():
+        return the_timezone
 
-    async def test_custom_async_locale_selector(self):
-        app = Quart(__name__)
-        b = babel_ext.Babel(app)
-        d = datetime(2010, 4, 12, 13, 46)
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == 'Aug 8, 2022, 5:46 PM'
 
-        the_timezone = 'UTC'
-        the_locale = 'en_US'
+    the_locale = 'de_DE'
+    the_timezone = 'Europe/Vienna'
 
-        @b.localeselector
-        async def select_locale():
-            await asyncio.sleep(0.1)
-            return the_locale
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == '08.08.2022, 17:46:00'
 
-        @b.timezoneselector
-        async def select_timezone():
-            await asyncio.sleep(0.2)
-            return the_timezone
+@pytest.mark.asyncio
+async def test_async_custom_locale_selector():
+    """
+    Tests a custom locale selector that is async.
+    """
+    app = Quart(__name__)
+    babel = Babel(app)
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
+    date = datetime(2022, 8, 8, 17, 46)
 
-        the_locale = 'de_DE'
-        the_timezone = 'Europe/Vienna'
+    the_locale = 'en_US'
+    the_timezone = 'UTC'
 
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == '12.04.2010, 15:46:00'
+    @babel.localeselector
+    async def select_locale():
+        await asyncio.sleep(0.1)
+        return the_locale
 
-    async def test_refreshing(self):
-        app = Quart(__name__)
-        babel_ext.Babel(app)
-        d = datetime(2010, 4, 12, 13, 46)
-        babel_ext.refresh()  # nothing should be refreshed (see case below)
-        async with app.test_request_context("/"):
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 1:46:00 PM'
-            app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europe/Vienna'
-            babel_ext.refresh()
-            assert babel_ext.format_datetime(d) == 'Apr 12, 2010, 3:46:00 PM'
+    @babel.timezoneselector
+    async def select_timezone():
+        await asyncio.sleep(0.2)
+        return the_timezone
 
-    async def test_force_locale(self):
-        app = Quart(__name__)
-        b = babel_ext.Babel(app)
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == 'Aug 8, 2022, 5:46 PM'
 
-        @b.localeselector
-        def select_locale():
-            return 'de_DE'
+    the_locale = 'de_DE'
+    the_timezone = 'Europe/Vienna'
 
-        with babel_ext.force_locale('en_US'):
-            assert babel_ext.get_locale() is None
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == '08.08.2022, 17:46:00'
 
-        async with app.test_request_context("/"):
-            assert str(babel_ext.get_locale()) == 'de_DE'
-            with babel_ext.force_locale('en_US'):
-                assert str(babel_ext.get_locale()) == 'en_US'
-            assert str(babel_ext.get_locale()) == 'de_DE'
+@pytest.mark.asyncio
+async def test_refreshing():
+    """
+    Test Quart Babel refreshing.
+    """
+    app = Quart(__name__)
+    Babel(app)
+
+    date = datetime(2022, 8, 8, 17, 46)
+
+    refresh() # nothing shoule be refreshed (see case below)
+
+    async with app.test_request_context("/"):
+        assert format_datetime(date) == 'Aug 8, 2022, 5:46:00 PM'
+        app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europe/Vienna'
+        refresh()
+        assert format_datetime(date) == 'Aug 8, 2022, 11:46:00 PM'
+
+@pytest.mark.asyncio
+async def test_force_locale():
+    """
+    Tests forcing the locale.
+    """
+    app = Quart(__name__)
+    babel = Babel(app)
+
+    @babel.localeselector
+    def select_locale():
+        return 'de_DE'
+
+    with force_locale('en_US'):
+        assert get_locale() is None
+
+    async with app.test_request_context("/"):
+        assert str(get_locale()) == 'de_DE'
+
+        with force_locale('en_US'):
+            assert str(get_locale()) == 'en_US'
+
+        assert str(get_locale()) == 'de_DE'
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,81 +1,100 @@
 """
-Test babel integration.
+Tests the core `quart_babel` integration.
 """
 from datetime import datetime
-import aiounittest
 import pytest
 from babel import Locale
 from pytz import timezone, UTC
 
 from quart import Quart
-import quart_babel as babel_ext
+from quart_babel import Babel, get_locale, get_timezone, to_utc, to_user_timezone
 from quart_babel.utils.context import get_state
 
+def test_configure_jinja():
+    """
+    Test `jinja` configuration in
+    `quart_babel`.
+    """
+    app = Quart(__name__)
+    Babel(app, configure_jinja=False)
+    assert not app.jinja_env.filters.get("scientificformat")
 
-class IntegrationTestCase(aiounittest.AsyncTestCase):
-    def test_configure_jinja(self):
-        app = Quart(__name__)
-        babel_ext.Babel(app, configure_jinja=False)
-        assert not app.jinja_env.filters.get("scientificformat")
+@pytest.mark.asyncio
+async def test_get_state():
+    """
+    Tests the `get_state` function.
+    """
+    # app = None, app.extensions = False, babel = False, silent = True
+    assert get_state(silent=True) is None
 
-    async def test_get_state(self):
-        # app = None; app.extensions = False; babel = False; silent = True;
-        assert get_state(silent=True) is None
+    app = Quart(__name__)
 
-        app = Quart(__name__)
-        with pytest.raises(RuntimeError):
-            async with app.test_request_context("/"):
-                # app = app; silent = False
-                # babel not in app.extensions
-                get_state()
+    with pytest.raises(RuntimeError):
+        async with app.test_request_context("/"):
+            # app = app, silent = False
+            # babel not in app.extensions
+            get_state()
 
-        # same as above, just silent
+    # same as above, just silent
+    with pytest.raises(RuntimeError):
         async with app.test_request_context("/"):
             assert get_state(app=app, silent=True) is None
 
-        babel_ext.Babel(app)
-        async with app.test_request_context("/"):
-            # should use current_app
-            assert get_state(app=None, silent=True) == app.extensions['babel']
+@pytest.mark.asyncio
+async def test_get_locale():
+    """
+    Tests the `get_locale` function.
+    """
+    app = Quart(__name__)
+    Babel(app)
 
-    async def test_get_locale(self):
-        assert babel_ext.get_locale() is None
+    async with app.app_context():
+        assert get_locale() == Locale.parse("en")
 
-        app = Quart(__name__)
-        babel_ext.Babel(app)
-        async with app.app_context():
-            assert babel_ext.get_locale() == Locale.parse("en")
+@pytest.mark.asyncio
+async def test_get_timezone_none():
+    """
+    Tests if `get_timezone` functions
+    if timezone is None.
+    """
+    app = Quart(__name__)
+    babel = Babel(app)
 
-    async def test_get_timezone_none(self):
-        assert babel_ext.get_timezone() is None
+    @babel.timezoneselector
+    def tz_none():
+        return None
 
-        app = Quart(__name__)
-        b = babel_ext.Babel(app)
+    async with app.test_request_context("/"):
+        assert get_timezone() == UTC
 
-        @b.timezoneselector
-        def tz_none():
-            return None
-        async with app.test_request_context("/"):
-            assert babel_ext.get_timezone() == UTC
+@pytest.mark.asyncio
+async def test_get_timezone_vienna():
+    """
+    Tests if the timezone is Vienna.
+    """
+    app = Quart(__name__)
+    babel = Babel(app)
 
-    async def test_get_timezone_vienna(self):
-        app = Quart(__name__)
-        b = babel_ext.Babel(app)
+    @babel.timezoneselector
+    def tz_vienna():
+        return timezone('Europe/Vienna')
 
-        @b.timezoneselector
-        def tz_vienna():
-            return timezone('Europe/Vienna')
-        async with app.test_request_context("/"):
-            assert babel_ext.get_timezone() == timezone('Europe/Vienna')
+    async with app.test_request_context("/"):
+        assert get_timezone() == timezone('Europe/Vienna')
 
-    async def test_convert_timezone(self):
-        app = Quart(__name__)
-        babel_ext.Babel(app)
-        dt = datetime(2010, 4, 12, 13, 46)
+@pytest.mark.asyncio
+async def test_convert_timezone():
+    """
+    Tests converting the timezone.
+    """
+    app = Quart(__name__)
+    Babel(app)
 
-        async with app.test_request_context("/"):
-            dt_utc = babel_ext.to_utc(dt)
-            assert dt_utc.tzinfo is None
+    date_time = datetime(2022, 8, 8, 17, 46)
 
-            dt_usertz = babel_ext.to_user_timezone(dt_utc)
-            assert dt_usertz is not None
+    async with app.test_request_context("/"):
+        dt_utc = to_utc(date_time)
+        assert dt_utc.tzinfo is None
+
+        dt_usertz = to_user_timezone(dt_utc)
+        assert dt_usertz is not None
