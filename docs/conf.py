d35a960b073ac98a76f6d176f665265dfd3318e8
Chris Rood <crood58@gmail.com>
2022-08-16 02:26:59 +0000

    Added sphinx files and logos.

---
+++ docs/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Quart-Babel'
+copyright = '2022, Chris Rood'
+author = 'Chris Rood'
+release = '0.0.2'
+version = release
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+master_doc = 'index'
+source_suffix = '.rst'
+
+autodoc_preserve_defaults = True
+
+autodoc_default_options = {
+    'member-order': 'bysource',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+htmlhelp_basename = 'QuartBabelDoc'
+
+html_logo = "_static/logo_short.png"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    "external_links": [
+        {"name": "Source code", "url": "https://github.com/Quart-Addons/quart-babel"},
+        {"name": "Issues", "url": "https://github.com/Quart-Addons/quart-babel/issues"},
+    ],
+    "icon_links": [
+        {
+            "name": "Quart Add-Ons",
+            "url": "https://github.com/Quart-Addons",
+            "icon": "fab fa-github",
+        },
+        {
+            "name": "Quart",
+            "url": "https://quart.palletsprojects.com/",
+            "icon": "_static/quart.png",
+            "type": "local",
+        },
+    ],
+}
